1ba54c85604c17d4da64ec48a14bec79286a78dc
Lullaby330 <69coder@gmail.com>
2024-09-04 10:23:42 +0700

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://makan:makan@cluster0.og6i
 DB_NAME = os.environ.get("DATABASE_NAME", "askeling")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002243011293"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002101526284"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
